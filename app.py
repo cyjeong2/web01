110dc8ec410927703770fe01d1d6e5ff2e27cd86
cyjeong2 <jcyjjang95@gmail.com>
2022-10-20 14:26:41 +0900

    22/10/20 오후 중간저장

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,7 +10,18 @@ def index():
 def blog() :
     return render_template('blog.html')
 
+@app.route('/test/<username>')
+def test(username) :
+    print(username)
+    return render_template('test_result.html', name = username)
 
+@app.route('/method', methods = ['GET', 'POST'])
+def method() :
+    if request.method == 'POST' :
+        print('Post')
+    else :
+        print('Get')
+    return request.method
 if __name__ == '__main__' :
     app.run(debug = True, port = 80)
 
