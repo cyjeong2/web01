526c3bf21e818070eb79759fc2bf3e183b56638b
cyjeong2 <jcyjjang95@gmail.com>
2022-10-21 17:08:36 +0900

    2022/10/21 마지막 결과물 저장

--- app.py
+++ app.py
@@ -54,6 +54,47 @@ def bloglist() :
     print(rows)
     return render_template('bloglist.html', data = rows)
 
+@app.route('/blogform', methods = ['GET', 'POST'])
+def blogform() :
+    if request.method == 'GET' :
+        return render_template('blogform.html')
+    else :
+        f = request.files['formFile']
+        path = os.path.dirname(__file__) + '/static/blog/img/' + f.filename
+        print(path)
+        f.save(path)
+        print('저장성공!!')
+        print(request.form)
+        conn = db.dbconn()
+        cursor = conn.cursor()
+        sql = '''insert into blog values(?,?,?)'''
+        data = [request.form['title'], request.form['content'], '/static/blog/img/' + f.filename]
+        cursor.execute(sql, data)
+        conn.commit()
+        conn.close()
+        return redirect('/bloglist')
+
+@app.route('/blog/<int:id>')
+def blogcontent(id) :
+    conn = db.dbconn() 
+    cursor = conn.cursor()
+    sql = '''select * from blog where id = ?'''
+    cursor.execute(sql, id)
+    rows = cursor.fetchone()
+    conn.close()
+    return render_template('blog_content.html', data = rows)
+
+
+
+@app.route('/blogdelete/<int:id>')
+def blogdelete(id) :
+    conn = db.dbconn() 
+    cursor = conn.cursor()
+    sql = '''delete blog where id = ?'''
+    cursor.execute(sql, id)
+    conn.commit()
+    conn.close()
+    return redirect('/bloglist')
 
 
 if __name__ == '__main__' :
